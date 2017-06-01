4d5d8abb53efdc45d049e85f325d95590ed80101
Christopher Workman <christopher.workman@milliman.com>
2017-05-31 22:52:35 -0700

    Adding BlogServicesWebApi project

---
+++ BlogDataServices/BlogWebApi/BlogWebApi/Controllers/BlogController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BlogWebApi.Models;
+
+namespace BlogWebApi.Controllers
+{
+    public class BlogController : ApiController
+    {
+        Blog[] blogs = new Blog[] {
+            new Blog { Id = 1, Author = "Chris Workman", Content = "Hello World", Type = "FrontEnd" }
+        };
+
+        [HttpGet]
+        [ActionName("blogs")]
+        public object GetBlog()
+        {
+            return new
+            {
+                blogList = blogs
+            };
+        }
+    }
+}
