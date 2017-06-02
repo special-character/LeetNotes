d4a65f7cc40fba143862d191371f1bb9b5a1d8eb
Christopher Workman <christopher.workman@milliman.com>
2017-06-01 23:52:19 -0700

    Reading from db via WebApi

--- BlogDataServices/BlogWebApi/BlogWebApi/Controllers/BlogController.cs
+++ BlogDataServices/BlogWebApi/BlogWebApi/Controllers/BlogController.cs
@@ -5,23 +5,62 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using BlogWebApi.Models;
+using System.Data.SqlClient; 
 
 namespace BlogWebApi.Controllers
 {
     public class BlogController : ApiController
     {
-        Blog[] blogs = new Blog[] {
-            new Blog { id = 1, author = "Chris Workman", content = "Hello World", blogType = "FrontEnd" }
-        };
+        List<Blog> blogs = new List<Blog> {};
 
         [HttpGet]
         [ActionName("blogs")]
         public object GetBlog()
         {
-            return new
-            {
+            var bls = this.queryBlogs();
+            return new {
                 blogList = blogs
             };
         }
+
+        public List<Blog> queryBlogs()
+        {
+
+            using (var conn = new SqlConnection("data source=localhost;database=MIEngineering;trusted_connection=true"))
+            {
+                try
+                {
+                    conn.Open();
+                    var command = new SqlCommand("Select * from dbo.BlogPosts", conn);
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var i = 0;
+                            var Id = reader.GetInt32(0);
+                            var Content = reader.GetString(1);
+                            var Author = reader.GetString(2);
+                            var BlogType = reader.GetString(3);
+
+                            blogs.Add(new Blog { id = Id , author = Author, content = Content, blogType = BlogType });
+                        }
+
+                        return blogs; 
+
+                    }
+
+                }
+                catch (SqlException ex)
+                {
+                    return blogs;
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+        }
+
     }
 }
