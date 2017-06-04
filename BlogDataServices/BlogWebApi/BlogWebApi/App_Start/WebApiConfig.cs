7e96d5880ecbe92b8557f5914074f70460b7e95d
Christopher Workman <christopher.workman@milliman.com>
2017-06-03 18:03:15 -0700

    Unindexing crap

---
+++ BlogDataServices/BlogWebApi/BlogWebApi/App_Start/WebApiConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors; 
+
+namespace BlogWebApi
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
+            config.Formatters.JsonFormatter.SerializerSettings.DefaultValueHandling =
+                                   Newtonsoft.Json.DefaultValueHandling.Include;
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{actions}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
